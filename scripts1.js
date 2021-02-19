64631f20ac768ffc78b5720fdf25be5c31ec7687
JWNicholson <john.nicholson812@gmail.com>
2021-02-19 10:41:09 -0500

    initial commit

---
+++ scripts1.js
@@ -0,0 +1,10 @@
+console.log("fuddge")
+
+const virgel = {
+    firstName: 'Virgel',
+    lastName: 'Smith',
+    birthYear: 1970,
+    job: 'collections',
+    friends: ['Smokestack', 'Creeper', 'Snake'],
+    hasDriversLicense: true,
+}
